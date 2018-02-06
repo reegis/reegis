6747eadd5b84e8ef0c07e23941be747d5ae5dcce
uwe <uwe.krien@rl-institut.de>
2018-02-06 12:24:50 +0100

    add simple setup file

---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='reegis_tools',
+      version='0.0.1',
+      author='Uwe Krien',
+      author_email='uwe.krien@rl-institut.de',
+      description='A local heat and power system',
+      package_dir={'reegis_tools': 'reegis_tools'},
+      install_requires=['oemof >= 0.1.0',
+                        'pandas >= 0.17.0',
+                        'demandlib',
+                        'tables',
+                        'matplotlib',
+                        'shapely',
+                        'windpowerlib',
+                        'pvlib',
+                        'tables',
+                        'geopandas',
+                        'requests',
+                        'numpy',
+                        'geoplot',
+                        'workalendar',
+                        'owslib', 'pyproj']
+      )
