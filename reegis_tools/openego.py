2b7090becdce15aca36378a8e7edbf2bfa2dee23
uwe <uvchik.git@posteo.eu>
2018-11-12 20:36:02 +0100

    Download ego data

--- reegis_tools/openego.py
+++ reegis_tools/openego.py
@@ -13,6 +13,8 @@ __license__ = "GPLv3"
 # Python libraries
 import os
 import logging
+import requests
+from shapely import wkb
 
 # External libraries
 import pandas as pd
@@ -20,6 +22,43 @@ import pandas as pd
 # Internal modules
 import reegis_tools.config as cfg
 import reegis_tools.geometries as geometries
+
+import oemof.tools.logger as logger
+
+
+def download_ego_data():
+
+    oep_url = 'http://oep.iks.cs.ovgu.de/api/v0'
+
+    local_path = cfg.get('paths', 'ego')
+
+    # Large scale consumer
+    schema = 'model_draft'
+    table = 'ego_demand_hv_largescaleconsumer'
+    query = ''
+    full_url = '{url}/schema/{schema}/tables/{table}/rows/{query}'.format(
+        url=oep_url, schema=schema, table=table, query=query)
+    logging.info("Download data set from {0}".format(full_url))
+    result = requests.get(full_url)
+    logging.info("Got results: {0}".format(result.status_code))
+    result_df = pd.DataFrame(result.json())
+    fn = os.path.join(local_path, 'ego_large_consumer.csv')
+    logging.info("Write data to {0}".format(fn))
+    result_df.to_csv(fn)
+
+    # Load areas
+    schema = 'demand'
+    table = 'ego_dp_loadarea'
+    query = '?where=version=v0.4.5'
+    full_url = '{url}/schema/{schema}/tables/{table}/rows/{query}'.format(
+        url=oep_url, schema=schema, table=table, query=query)
+    logging.info("Download data set from {0}".format(full_url))
+    result = requests.get(full_url)
+    logging.info("Got results: {0}".format(result.status_code))
+    result_df = pd.DataFrame(result.json())
+    fn = os.path.join(local_path, 'ego_load_areas.csv')
+    logging.info("Write data to {0}".format(fn))
+    result_df.to_csv(fn)
 
 
 def prepare_ego_demand(egofile):
@@ -60,6 +99,8 @@ def get_ego_demand(overwrite=False):
 
 
 if __name__ == "__main__":
+    logger.define_logging()
+    download_ego_data()
     ego = get_ego_demand().groupby('federal_states').sum()[
         'sector_consumption_sum']
     print(ego)
