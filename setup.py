6d46699812bafed83268c64ce6a0e90b4e16b9c9
uwe <uvchik.git@posteo.eu>
2019-01-17 13:41:10 +0100

    Limit pandas version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if not os.environ.get('READTHEDOCS') == 'True':
         'PyQt5',
         'cython']
 else:
-    requirements = ['oemof', 'pandas', 'pvlib', 'requests']
+    requirements = ['oemof', 'pandas < 0.24', 'pvlib', 'requests']
 
 
 setup(name='reegis',
