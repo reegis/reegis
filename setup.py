ca63f8bc16055d28312d3118c4669cfe2455fdf7
uwe <uvchik.git@posteo.eu>
2019-01-17 11:32:10 +0100

    Remove geopandas from requirements for readthedocs

--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
 #! /usr/bin/env python
 
 from setuptools import setup
+import os
+
+requirements = [
+    'oemof >= 0.1.0',
+    'pandas >= 0.17.0',
+    'demandlib',
+    'tables',
+    'matplotlib',
+    'shapely',
+    'windpowerlib',
+    'pvlib==v0.6.1-beta',
+    'geopandas',
+    'requests',
+    'numpy >= 0.16',
+    'geoplot',
+    'workalendar',
+    'owslib',
+    'pyproj',
+    'pytz',
+    'python-dateutil',
+    'networkx',
+    'dill',
+    'PyQt5',
+    'cython']
+
+if os.environ.get('READTHEDOCS') == 'True':
+    requirements.remove('geopandas')
 
 setup(name='reegis',
       version='0.0.1',
@@ -8,26 +35,4 @@ setup(name='reegis',
       author_email='uwe.krien@posteo.eu',
       description='Open geospatial data model',
       package_dir={'reegis': 'reegis'},
-      install_requires=['cython',
-                        'oemof >= 0.1.0',
-                        'pandas >= 0.17.0',
-                        'demandlib',
-                        'tables',
-                        'matplotlib',
-                        'shapely',
-                        'windpowerlib',
-                        'pvlib==v0.6.1-beta',
-                        'geopandas',
-                        'requests',
-                        'numpy >= 0.16',
-                        'geoplot',
-                        'workalendar',
-                        'owslib',
-                        'pyproj',
-                        'pytz',
-                        'python-dateutil',
-                        'networkx',
-                        'dill',
-                        'PyQt5',
-                        'cython'])
-
+      install_requires=requirements)
