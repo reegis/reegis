bbb47029cd04d2a22f8c70a960fe3d5064ccb88b
nesnoj <jonathan.amme@rl-institut.de>
2019-02-06 14:01:41 +0100

    move leap year check to correct place

--- reegis/coastdat.py
+++ reegis/coastdat.py
@@ -934,15 +934,15 @@ def scenario_feedin(year, name, weather_year=None, feedin_ts=None):
     for region in geothermal.columns:
         feedin_ts[region, 'geothermal'] = geothermal[region]
 
+    if calendar.isleap(year) and weather_year is not None:
+        if not calendar.isleap(weather_year):
+            feedin_ts = feedin_ts.iloc[:8760]
+
     feedin_ts = scenario_feedin_pv(year, name, feedin_ts=feedin_ts,
                                    weather_year=weather_year)
 
     feedin_ts = scenario_feedin_wind(year, name, feedin_ts=feedin_ts,
                                      weather_year=weather_year)
-
-    if calendar.isleap(year) and weather_year is not None:
-        if not calendar.isleap(weather_year):
-            feedin_ts = feedin_ts.iloc[:8760]
 
     return feedin_ts.sort_index(1)
 
