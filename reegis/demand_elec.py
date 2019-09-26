1dcfca1dcdec79e156b51151a55edde5f25e801d
uvchik <uvchik.git@posteo.eu>
2019-09-26 12:13:03 +0200

    Move not working function to feature-brnach

--- reegis/demand_elec.py
+++ reegis/demand_elec.py
@@ -26,59 +26,6 @@ from reegis import openego
 from reegis import bmwi as bmwi_data
 
 
-def create_deflex_slp_profile(region, year, annual, profile='entsoe'):
-
-    demand_deflex = prepare_ego_demand()
-
-    cal = Germany()
-    holidays = dict(cal.holidays(year))
-
-    deflex_profile = pd.DataFrame()
-
-    for region in demand_deflex.index:
-        annual_demand = demand_deflex.loc[region]
-
-        annual_electrical_demand_per_sector = {
-            'g0': annual_demand.sector_consumption_retail,
-            'h0': annual_demand.sector_consumption_residential,
-            'l0': annual_demand.sector_consumption_agricultural,
-            'i0': annual_demand.sector_consumption_industrial}
-        e_slp = bdew.ElecSlp(year, holidays=holidays)
-
-        elec_demand = e_slp.get_profile(
-            annual_electrical_demand_per_sector)
-
-        # Add the slp for the industrial group
-        ilp = profiles.IndustrialLoadProfile(e_slp.date_time_index,
-                                             holidays=holidays)
-
-        elec_demand['i0'] = ilp.simple_profile(
-            annual_electrical_demand_per_sector['i0'])
-
-        deflex_profile[region] = elec_demand.sum(1).resample('H').mean()
-    deflex_profile.to_csv(outfile)
-
-
-def get_slp_profile_by_region(region, year, annual, profile='entsoe'):
-    outfile = os.path.join(
-        cfg.get('paths', 'demand'),
-        cfg.get('demand', 'ego_profile_pattern').format(
-            year=year, map=cfg.get('init', 'map')))
-    if not os.path.isfile(outfile) or overwrite:
-        create_deflex_slp_profile(year, outfile)
-
-    deflex_profile = pd.read_csv(
-        outfile, index_col=[0], parse_dates=True,
-        date_parser=lambda col: pd.to_datetime(col, utc=True)).multiply(
-        1000)
-
-    if annual_demand is not None:
-        deflex_profile = deflex_profile.div(deflex_profile.sum().sum()
-                                            ).multiply(annual_demand)
-
-    return deflex_profile
-
-
 def get_entsoe_profile_by_region(region, year, name, annual_demand=None):
     """
 
