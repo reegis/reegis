9188bf16a9c20c205af41fd6de941a349b114edb
uvchik <uvchik.git@posteo.eu>
2019-10-25 13:13:02 +0200

    Revise plot function

--- reegis/dev/figures.py
+++ reegis/dev/figures.py
@@ -4,36 +4,51 @@ from matplotlib.colors import LinearSegmentedColormap
 from reegis import geometries
 
 
-def fig_deflex_de22_polygons():
+def fig_federal_states_polygons():
+    """Plot federal states as map."""
     ax = plt.figure(figsize=(9, 7)).add_subplot(1, 1, 1)
 
     # change for a better/worse resolution (
     simple = 0.02
 
     cmap = LinearSegmentedColormap.from_list(
-        'mycmap', [(0.000000000, '#badd69'),
-                   (0.5, '#dd5500'),
-                   (1, '#a5bfdd')])
+        'mycmap', [(0, '#badd69'), (1, '#a5bfdd')], 2)
 
     fs = geometries.get_federal_states_polygon()
+
+    # drop buffer region
     fs.drop('P0', axis=0, inplace=True)
-    fs['color'] = 0.5
-    fs.loc[['O0', 'N0', 'N1'], 'color'] = 1
+
+    # dissolve offshore regions to one region
+    fs['region'] = fs.index   # prevent index
     fs.loc[['O0', 'N0', 'N1'], 'SN_L'] = '00'
-    fs['region'] = fs.index
     fs = fs.dissolve(by='SN_L')
     fs.loc['00', 'region'] = 'AW'
-    fs['geometry'] = fs['geometry'].simplify(simple)
-    # fs.loc['awz'] = fs.loc['N0'].union(fs.loc['N1'])
-    fs.plot(ax=ax, cmap=cmap, column='color', edgecolor='#888888')
-    plt.subplots_adjust(right=1, left=0, bottom=0, top=1)
+    fs.loc['00', 'name'] = 'Ausschließliche Wirtschaftszone'
+    fs.set_index('region', inplace=True)  # write back index
 
+    # simplify the geometry to make the resulting file smaller
+    fs['geometry'] = fs['geometry'].simplify(simple)
+
+    # set color column
+    fs['color'] = 0
+    fs.loc['AW', 'color'] = 1
+
+    # plot map
+    fs.plot(ax=ax, cmap=cmap, column='color', edgecolor='#888888')
+
+    # adjust plot
+    plt.subplots_adjust(right=1, left=0, bottom=0, top=1)
     ax.set_axis_off()
+
+    # write figure to file
     fn = os.path.join(os.path.dirname(__file__),
                       os.pardir, os.pardir, 'docs', '_files',
                       'federal_states_region_plot.svg')
     plt.savefig(fn)
+    # print(fs[['name', 'color', 'geometry']])
     plt.show()
 
 
-fig_deflex_de22_polygons()
+if __name__ == "__main__":
+    fig_federal_states_polygons()
